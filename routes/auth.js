7b3c56924e400d2509f96f78835a31835cbec9d6
Foxxy Studio <hello+github@foxxy.studio>
2025-12-24 22:18:56 +0400

    cookie settings changed 2

--- routes/auth.js
+++ routes/auth.js
@@ -14,7 +14,7 @@ const tokenCookieOptions = {
    sameSite: isProduction ? 'none' : 'lax',
    path: '/',
    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-   domain: isProduction ? process.env.CLIENT_URL_PROD : process.env.CLIENT_URL_DEV,
+   domain: isProduction ? process.env.COOKIE_DOMAIN : undefined,
 };
 
 // JWT auth middleware for protected routes
