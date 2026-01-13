9b46453aac4f7e539071f711b6238a7c38b1ce02
Foxxy Studio <hello+github@foxxy.studio>
2026-01-13 05:03:27 +0400

    droplet github test

--- server.js
+++ server.js
@@ -41,6 +41,10 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+   res.send('Hello World');
+});
+
 app.listen(PORT, () => {
    console.log(`Server started on port ${PORT}`);
 });
